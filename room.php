6f64ea74dfc8839f6aaea1ad52b0a0966a20452c
Ryo <abejadrcs@gmail.com>
2021-06-16 13:54:51 +0700

    [ADD] a href to lab

--- room.php
+++ room.php
@@ -65,7 +65,7 @@
                         </div>
                         <div class="right hide-on-med-and-down">
                             <a class='dropdown-trigger btn' href='#' data-target='playlist'>Basic Web Security</a>
-                            <a class="waves-effect waves-light btn">Course Lab</a>
+                            <a class="waves-effect waves-light btn" href="lab/lab_basic-security_admin.php">Course Lab</a>
                         </div>
                         <a href="#" data-target="mobile-nav" class="sidenav-trigger"><i class="material-icons white-text">menu</i></a>
                     </div>
