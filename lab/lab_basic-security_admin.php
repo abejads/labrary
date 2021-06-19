ef19afedb408937f140e355254a5584b779845a6
Ryo <abejadrcs@gmail.com>
2021-06-19 09:58:54 +0700

    [ADD] certificate when finish lab

--- lab/lab_basic-security_admin.php
+++ lab/lab_basic-security_admin.php
@@ -1,4 +1,38 @@
 <?php
+	session_start();
+	if(!isset($_SESSION["uid"])){
+	    header("Location: index.php");
+	    die();
+	}
+
+	require_once("../include/connection.php");
+
+	$file = basename($_SERVER['SCRIPT_FILENAME']);
+	$doQuery = $conn->prepare("SELECT * FROM rooms JOIN labs ON rooms.labID = labs.labID WHERE labs.labPath LIKE CONCAT('%',?,'%')");
+
+	if($doQuery->bind_param("s", $file) && $doQuery->execute()){
+		$result = $doQuery->get_result();
+		while ($course = $result->fetch_assoc()){
+			$courseID = $course['courseID'];
+			break;
+		}
+
+		$doQuery = $conn->prepare("SELECT * FROM certificates WHERE courseID = ?");
+
+		if($doQuery->bind_param("i", $courseID) && $doQuery->execute()){
+			$result = $doQuery->get_result();
+			if($result->num_rows > 0){
+				echo '<script>alert("You have been completed this lab, try another lab!"); window.location = "course.php";</script>';
+			}
+		}
+	
+	} else {
+		echo '<script>alert("Error!")<script>';
+        die();
+	}
+
+?>
+<?php
 	if(isset($_COOKIE['disabled'])){
 		if(isset($_POST['logout'])){
 			unset($_COOKIE['disabled']);
@@ -7,6 +41,20 @@
 		} else if(strtolower($_COOKIE['disabled']) == 'false'){
 			$error = 100;
 
+			$certID = md5(uniqid(time(), true));
+			$doQuery = $conn->prepare("INSERT INTO certificates VALUES(?, ?, ?)");
+
+			if($doQuery->bind_param("sii", $certID, $courseID, $_SESSION['uid']) && $doQuery->execute()){
+
+				$certID = $certID;
+
+			} else {
+				echo '<script>alert("Error!")<script>';
+        		die();
+			}
+
+
+
 		} else {
 			$error = 1;	
 
@@ -14,7 +62,7 @@
 
 	} else if(isset($_POST['username']) && $_POST['username'] != "" && isset($_POST['password']) && $_POST['password'] != ""){
 		if($_POST['username'] == 'admin' && $_POST['password'] == 'admin'){
-			setcookie("disabled","true", time() + (86400 * 30), "/");
+			setcookie("disabled","true", time() + (3600 * 30), "/");
 			$error = 1;
 		} else {
 			$error = 2;
@@ -31,8 +79,8 @@
 	</head>
 	<body>
 	<center>
-		<h1 style="margin-bottom: 10%;">This flag for you</h1>
-		<h1>L4bR@Ry{c00k13_m4N1pUl4t10n}</h1>
+		<h1 style="margin-bottom: 10%;">Congratulations!</h1>
+		<h1>You completed this lab, heres your <a href="../certificate.php?id=<?php echo certID; ?>">certificates</a></h1>
 	</center>
 
 	</body>
@@ -62,7 +110,7 @@
 	<body>
 	<center>
 		<h1>Admin Login Area</h1>
-		<p style="margin-bottom: 10%;">You must logged in wtih admin account to access the flag
+		<p style="margin-bottom: 10%;">You must logged in with admin account to access the flag
 		<form method="POST">
 			<label for="username">Username: </label>
 			<input type="text" name="username" id="username">
